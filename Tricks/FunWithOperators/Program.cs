aa137882ee2d0d55957149c268a918e1b87e5240
Berial <misiek767@gmail.com>
2015-04-23 19:11:02 +0200

    Added fun with operators

---
+++ Tricks/FunWithOperators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyStruct();
+        }
+    }
+
+    public class MyClass
+    {
+        
+    }
+
+    public struct MyStruct
+    {
+        public static implicit operator MyClass(MyStruct myStruct)
+        {
+            return new MyClass();
+        }
+    }
+}
