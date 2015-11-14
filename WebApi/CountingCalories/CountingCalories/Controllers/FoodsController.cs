c1ecf5b338c033c0f49e7be97637b178b2677a08
tpodolak <tom.podolak@gmail.com>
2015-11-14 19:44:32 +0100

    Playing around with WebApi - initial commit

---
+++ WebApi/CountingCalories/CountingCalories/Controllers/FoodsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using CountingKs.Data;
+using CountingKs.Data.Entities;
+
+namespace CountingKs.Controllers
+{
+    public class FoodsController : ApiController
+    {
+        private readonly ICountingKsRepository countingKsRepo;
+
+        public FoodsController(ICountingKsRepository countingKsRepo)
+        {
+            this.countingKsRepo = countingKsRepo;
+        }
+
+        public IEnumerable<Food> Get()
+        {
+            var results = this.countingKsRepo.GetAllFoods()
+                .OrderBy(val => val.Description)
+                .Take(25)
+                .ToList();
+
+            return results;
+        }
+    }
+}
---
+++ WebApi/CountingCalories/CountingCalories/Global.asax.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.WebApi;
+using CountingKs.Data;
+
+namespace CountingKs
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
+            ConfigureContainer();
+        }
+
+        private void ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            var dataAssembly = Assembly.GetAssembly(typeof(ICountingKsRepository));
+            builder.RegisterApiControllers(executingAssembly);
+            builder.RegisterAssemblyTypes(executingAssembly, dataAssembly).AsImplementedInterfaces();
+            builder.RegisterAssemblyTypes(executingAssembly, dataAssembly);
+            var container = builder.Build();
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
