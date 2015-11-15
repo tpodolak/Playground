2b68cb3273eb1f3750af41c1d365ee51b68058aa
tpodolak <tom.podolak@gmail.com>
2015-11-15 15:32:44 +0100

    Paging

--- WebApi/CountingCalories/CountingCalories/Controllers/FoodsController.cs
+++ WebApi/CountingCalories/CountingCalories/Controllers/FoodsController.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Routing;
 using CountingKs.Data;
 using CountingKs.Data.Entities;
 using CountingKs.Infrastructure;
@@ -11,21 +13,34 @@ namespace CountingKs.Controllers
     {
         private readonly ICountingKsRepository countingKsRepo;
         private readonly IModelFactory modelFactory;
+        private const int PageSize = 50;
 
-        public FoodsController(ICountingKsRepository countingKsRepo,IModelFactory modelFactory)
+        public FoodsController(ICountingKsRepository countingKsRepo, IModelFactory modelFactory)
         {
             this.countingKsRepo = countingKsRepo;
             this.modelFactory = modelFactory;
         }
 
-        public IEnumerable<FoodModel> Get(bool includeMeasures = true)
+        public PageResult<FoodModel> Get(bool includeMeasures = true, int page = 0)
         {
-            IQueryable<Food> query;
-            query = includeMeasures ? countingKsRepo.GetAllFoodsWithMeasures() : countingKsRepo.GetAllFoods();
-            query = query.OrderBy(val => val.Description)
-                         .Take(25);
+            var urlHelper = new UrlHelper(Request);
 
-            return modelFactory.Create(query);
+            var query = includeMeasures ? countingKsRepo.GetAllFoodsWithMeasures() : countingKsRepo.GetAllFoods();
+            query = query.OrderBy(val => val.Description);
+
+            var totalCount = query.Count();
+            var totalPages = (int)Math.Ceiling((double)totalCount / PageSize);
+            var resultQuery = query.Skip(page * PageSize)
+                                   .Take(PageSize);
+
+            var results = modelFactory.Create(resultQuery);
+
+            var pageResult = new PageResult<FoodModel>(results, totalPages, totalCount)
+            {
+                PreviousPageUrl = page > 0 ? urlHelper.Link("Foods", new { page = page - 1 }) : string.Empty,
+                NextPageUrl = page < totalPages - 1 ? urlHelper.Link("Foods", new { page = page + 1 }) : string.Empty
+            };
+            return pageResult;
         }
 
         public FoodModel Get(int foodid)
---
+++ WebApi/CountingCalories/CountingCalories/Models/PageResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace CountingKs.Models
+{
+    public class PageResult<T>
+    {
+        public int TotalPages { get; private set; }
+        public int TotalCount { get; private set; }
+        public string PreviousPageUrl { get; set; }
+        public string NextPageUrl { get; set; }
+        public IEnumerable<T> Results { get; private set; }
+
+        public PageResult(IEnumerable<T> results, int totalPages, int totalCount)
+        {
+            Results = results;
+            TotalCount = totalCount;
+            TotalPages = totalPages;
+        }
+    }
+}
