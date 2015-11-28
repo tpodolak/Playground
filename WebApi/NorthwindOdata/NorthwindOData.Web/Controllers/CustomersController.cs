064b41c6c2b13c9b25b8d64e812159ed27bf66e2
tpodolak <tom.podolak@gmail.com>
2015-11-28 16:51:14 +0100

    CRUD operation with OData

--- WebApi/NorthwindOdata/NorthwindOData.Web/Controllers/CustomersController.cs
+++ WebApi/NorthwindOdata/NorthwindOData.Web/Controllers/CustomersController.cs
@@ -1,5 +1,9 @@
-﻿using System.Linq;
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
 using System.Web.Http.OData;
+using System.Web.Http.OData.Results;
 using NorthwindOData.Data;
 using NorthwindOData.Entities;
 
@@ -29,6 +33,44 @@ namespace NorthwindOData.Web.Controllers
         public IQueryable<Order> GetOrdersFromCustomer([FromODataUri] string key)
         {
             return _Context.Orders.Where(val => val.CustomerID == key);
+        }
+
+        protected override Customer UpdateEntity(string key, Customer update)
+        {
+            update.CustomerID = key;
+            _Context.Customers.Attach(update);
+            _Context.Entry(update).State = EntityState.Modified;
+            _Context.SaveChanges();
+            return update;
+        }
+
+        protected override Customer PatchEntity(string key, Delta<Customer> patch)
+        {
+            var customer = _Context.Customers.FirstOrDefault(val => val.CustomerID == key);
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            patch.Patch(customer);
+            _Context.SaveChanges();
+            return customer;
+        }
+
+        protected override Customer CreateEntity(Customer entity)
+        {
+            _Context.Customers.Add(entity);
+            _Context.SaveChanges();
+            return entity;
+        }
+
+        public override void Delete([FromODataUri] string key)
+        {
+            var customer = _Context.Customers.FirstOrDefault(val => val.CustomerID == key);
+            _Context.Customers.Remove(customer);
+            _Context.SaveChanges();
+        }
+
+        protected override string GetKey(Customer entity)
+        {
+            return entity.CustomerID;
         }
 
         protected override void Dispose(bool disposing)
