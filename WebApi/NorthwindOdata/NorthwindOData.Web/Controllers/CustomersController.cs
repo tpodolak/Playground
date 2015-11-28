45af4a21dd2831a8284fdd8e6823d3046dbaa9dd
tpodolak <tom.podolak@gmail.com>
2015-11-28 17:26:29 +0100

    Request validadtion

--- WebApi/NorthwindOdata/NorthwindOData.Web/Controllers/CustomersController.cs
+++ WebApi/NorthwindOdata/NorthwindOData.Web/Controllers/CustomersController.cs
@@ -3,7 +3,9 @@ using System.Linq;
 using System.Net;
 using System.Web.Http;
 using System.Web.Http.OData;
+using System.Web.Http.OData.Extensions;
 using System.Web.Http.OData.Results;
+using Microsoft.Data.OData;
 using NorthwindOData.Data;
 using NorthwindOData.Entities;
 
@@ -11,61 +13,65 @@ namespace NorthwindOData.Web.Controllers
 {
     public class CustomersController : EntitySetController<Customer, string>
     {
-        NorthwindDbContext _Context = new NorthwindDbContext();
+        readonly NorthwindDbContext context = new NorthwindDbContext();
 
         public CustomersController()
         {
-            _Context.Configuration.LazyLoadingEnabled = false;
+            context.Configuration.LazyLoadingEnabled = false;
         }
 
         [EnableQuery]
         public override IQueryable<Customer> Get()
         {
-            return _Context.Customers;
+            return context.Customers;
         }
 
         protected override Customer GetEntityByKey(string key)
         {
-            return _Context.Customers.FirstOrDefault(val => val.CustomerID == key);
+            return context.Customers.FirstOrDefault(val => val.CustomerID == key);
         }
 
         [EnableQuery]
         public IQueryable<Order> GetOrdersFromCustomer([FromODataUri] string key)
         {
-            return _Context.Orders.Where(val => val.CustomerID == key);
+            return context.Orders.Where(val => val.CustomerID == key);
         }
 
         protected override Customer UpdateEntity(string key, Customer update)
         {
+            EnsureEntityExists(key);
+
             update.CustomerID = key;
-            _Context.Customers.Attach(update);
-            _Context.Entry(update).State = EntityState.Modified;
-            _Context.SaveChanges();
+            context.Customers.Attach(update);
+            context.Entry(update).State = EntityState.Modified;
+            context.SaveChanges();
             return update;
         }
 
         protected override Customer PatchEntity(string key, Delta<Customer> patch)
         {
-            var customer = _Context.Customers.FirstOrDefault(val => val.CustomerID == key);
+            EnsureEntityExists(key);
+            var customer = context.Customers.FirstOrDefault(val => val.CustomerID == key);
             if (customer == null)
                 throw new HttpResponseException(HttpStatusCode.NotFound);
             patch.Patch(customer);
-            _Context.SaveChanges();
+            context.SaveChanges();
             return customer;
         }
 
         protected override Customer CreateEntity(Customer entity)
         {
-            _Context.Customers.Add(entity);
-            _Context.SaveChanges();
+            context.Customers.Add(entity);
+            context.SaveChanges();
             return entity;
         }
 
         public override void Delete([FromODataUri] string key)
         {
-            var customer = _Context.Customers.FirstOrDefault(val => val.CustomerID == key);
-            _Context.Customers.Remove(customer);
-            _Context.SaveChanges();
+            EnsureEntityExists(key);
+            var customer = context.Customers.FirstOrDefault(val => val.CustomerID == key);
+            context.Customers.Remove(customer);
+            context.SaveChanges();
         }
 
         protected override string GetKey(Customer entity)
@@ -76,8 +82,19 @@ namespace NorthwindOData.Web.Controllers
         protected override void Dispose(bool disposing)
         {
             base.Dispose(disposing);
-            _Context.Dispose();
+            context.Dispose();
         }
 
+        private void EnsureEntityExists(string key)
+        {
+            if (!context.Customers.Any(val => val.CustomerID == key))
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, new ODataError
+                {
+                    ErrorCode = "EntityNotFound",
+                    Message = $"Customer key {key} not found"
+                }));
+            }
+        }
     }
 }
