eeef8a6de72bddc5f1de29139e6c6ebe4f89d951
tpodolak <tom.podolak@gmail.com>
2015-12-12 00:27:35 +0100

    Simple unit tests for custom activity

---
+++ WF/IntroducingWF/IntroducingWF.Tests/CustomActivitiesTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Activities;
+using System.Collections.Generic;
+using System.Net.Mail;
+using IntroducingWF.CustomActivities;
+using NUnit.Framework;
+
+namespace IntroducingWF.Tests
+{
+    [TestFixture]
+    public class CustomActivitiesTests
+    {
+        [Test]
+        public void NotifyManager_SendsEmail_Test()
+        {
+            var resultKey = "emailResult";
+            var inputs = new Dictionary<string, object>
+            {
+                ["employeeEmail"] = "john@john.com"
+            };
+
+            var notifyManager = new NotifyManager();
+
+            var result = WorkflowInvoker.Invoke(notifyManager, inputs);
+            Assert.IsTrue(result.ContainsKey(resultKey));
+            Assert.IsInstanceOf<SmtpStatusCode>(result[resultKey]);
+            Assert.AreEqual(SmtpStatusCode.Ok, result[resultKey]);
+        }
+    }
+}
