ec2838d14296c2d10c688ad986d69d539eb9cd4a
tpodolak <tom.podolak@gmail.com>
2015-12-12 00:49:40 +0100

    Simple generic CodeActivity

---
+++ WF/IntroducingWF/IntroducingWF.CustomActivities/GetFeed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Activities;
+using System.ServiceModel.Syndication;
+
+namespace IntroducingWF.CustomActivities
+{
+    public class GetFeed : CodeActivity<SyndicationFeed>
+    {
+        [RequiredArgument]
+        public InArgument<string> FeedUrl { get; set; }
+
+        protected override SyndicationFeed Execute(CodeActivityContext context)
+        {
+            return new SyndicationFeed("title", "description", new Uri(FeedUrl.Get(context)));
+        }
+    }
+}
--- WF/IntroducingWF/IntroducingWF.Tests/CustomActivitiesTests.cs
+++ WF/IntroducingWF/IntroducingWF.Tests/CustomActivitiesTests.cs
@@ -1,6 +1,8 @@
 ﻿using System.Activities;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Mail;
+using System.ServiceModel.Syndication;
 using IntroducingWF.CustomActivities;
 using NUnit.Framework;
 
@@ -24,6 +26,22 @@ namespace IntroducingWF.Tests
             Assert.IsTrue(result.ContainsKey(resultKey));
             Assert.IsInstanceOf<SmtpStatusCode>(result[resultKey]);
             Assert.AreEqual(SmtpStatusCode.Ok, result[resultKey]);
+        }
+
+        [Test]
+        public void GetFeed_ReturnsCorrectAmountOfResults_Test()
+        {
+            var feedActivity = new GetFeed();
+            var feedUrl = "http://google.com/";
+            var inputs = new Dictionary<string, object>
+            {
+                ["FeedUrl"] = feedUrl
+            };
+
+            var feed = WorkflowInvoker.Invoke(feedActivity, inputs);
+            Assert.IsNotNull(feed);
+            Assert.AreEqual(1, feed.Links.Count);
+            Assert.AreEqual(feedUrl, feed.Links.Select(val => val.Uri.AbsoluteUri).Single());
         }
     }
 }
