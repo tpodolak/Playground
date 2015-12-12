9d687d8b8731e23ac89ed4a679a0a09fc3eeb361
tpodolak <tom.podolak@gmail.com>
2015-12-12 21:15:31 +0100

    SqlWorkflowInstanceStore configured

---
+++ WF/WCFIntegration/WCFIntegration.SelfHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+using System.ServiceModel.Activities;
+using System.Xaml;
+
+namespace WCFIntegration.SelfHost
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkflowServiceHost host = new WorkflowServiceHost(new ExpenseService(), new Uri("http://localhost:8731/Samples/Expenses"),
+                new Uri("net.tcp://localhost:8732/Samples/Expenses"));
+            host.Open();
+            Console.WriteLine("Listening");
+            Console.ReadKey();
+            host.Close();
+        }
+    }
+}
---
+++ WF/WCFIntegration/WCFIntegration/README.md
@@ -0,0 +1,3 @@
+Learning basics of Windows Workflow Foundation through Pluralsight examples
+
+[SQL Persistance for Workflows](https://msdn.microsoft.com/en-us/library/ee395773(v=vs.110).aspx)
