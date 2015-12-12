a3020333be5c7a96e053eda88553f6888cf4f56e
tpodolak <tom.podolak@gmail.com>
2015-12-12 01:03:42 +0100

    Playing around with AsyncCodeActivities

---
+++ WF/IntroducingWF/IntroducingWF.CustomActivities/GetFeedAsync.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Activities;
+using System.Net;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace IntroducingWF.CustomActivities
+{
+    public class GetFeedAsync : AsyncCodeActivity<SyndicationFeed>
+    {
+        public InArgument<string> FeedUrl { get; set; }
+        protected override IAsyncResult BeginExecute(AsyncCodeActivityContext context, AsyncCallback callback, object state)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(FeedUrl.Get(context));
+            context.UserState = request;
+            return request.BeginGetResponse(callback, state);
+        }
+
+        protected override SyndicationFeed EndExecute(AsyncCodeActivityContext context, IAsyncResult result)
+        {
+            HttpWebRequest request = (HttpWebRequest) context.UserState;
+            var endGetResponse = (HttpWebResponse)request.EndGetResponse(result);
+
+            return SyndicationFeed.Load(XmlReader.Create(endGetResponse.GetResponseStream()));
+        }
+    }
+}
