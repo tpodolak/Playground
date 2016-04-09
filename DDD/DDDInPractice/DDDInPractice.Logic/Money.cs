e8aa5915250daf207897892c39cb344c3560e7f8
tpodolak <tom.podolak@gmail.com>
2016-04-10 01:21:58 +0200

    DDDInPractice

---
+++ DDD/DDDInPractice/DDDInPractice.Logic/Money.cs
@@ -0,0 +1,28 @@
+﻿namespace DDDInPractice.Logic
+{
+	public class Money
+	{
+		public int OneCentCount { get; set; }
+		public int TenCentCount { get; set; }
+		public int QuaterCount { get; set; }
+		public int OneDollarCount { get; set; }
+		public int FiveDollarCount { get; set; }
+		public int TwentyDollarCount { get; set; }
+
+		public Money(int oneCentCount, int tenCentCount, int quaterCount, int oneDollarCount, int fiveDollarCount, int twentyDollarCount)
+		{
+			OneCentCount = oneCentCount;
+			TenCentCount = tenCentCount;
+			QuaterCount = quaterCount;
+			OneDollarCount = oneDollarCount;
+			FiveDollarCount = fiveDollarCount;
+			TwentyDollarCount = twentyDollarCount;
+		}
+
+		public static Money operator +(Money left, Money right)
+		{
+			return new Money(left.OneCentCount + right.OneCentCount, left.TenCentCount + right.TenCentCount, left.QuaterCount + right.QuaterCount,
+				left.OneDollarCount + right.OneDollarCount, left.FiveDollarCount + right.FiveDollarCount, left.TwentyDollarCount + right.TwentyDollarCount);
+		}
+	}
+}
---
+++ DDD/DDDInPractice/DDDInPractice.Logic/SnackMachine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DDDInPractice.Logic
+{
+	public class SnackMachine
+	{
+		public Money MoneyInside { get; set; }
+		public Money MoneyInTransaction { get; set; }
+
+		public void InsertMoney(Money money)
+		{
+			MoneyInTransaction += money;
+		}
+
+		public void BuySnack()
+		{
+			MoneyInside += MoneyInTransaction;
+		}
+
+		public void ReturnMoney()
+		{
+			// MoneyInTransaction = 0x0;
+		}
+	}
+}
+
+
