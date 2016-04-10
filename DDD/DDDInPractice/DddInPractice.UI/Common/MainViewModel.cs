7fe5c63839e1d832542f4f90de7e5726aa19a067
tpodolak <tom.podolak@gmail.com>
2016-04-10 14:54:54 +0200

    Adding UI layer

---
+++ DDD/DDDInPractice/DddInPractice.UI/Common/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using DddInPractice.Logic;
+using DDDInPractice.Logic;
+using NHibernate;
+
+namespace DddInPractice.UI.Common
+{
+    public class MainViewModel : ViewModel
+    {
+        public MainViewModel()
+        {
+            var viewModel = new SnackMachineViewModel(new SnackMachine());
+            _dialogService.ShowDialog(viewModel);
+        }
+    }
+}
---
+++ DDD/DDDInPractice/DddInPractice.UI/SnackMachineViewModel.cs
@@ -0,0 +1,18 @@
+﻿using DddInPractice.Logic;
+using DddInPractice.UI.Common;
+using DDDInPractice.Logic;
+using NHibernate;
+
+namespace DddInPractice.UI
+{
+    public class SnackMachineViewModel : ViewModel
+    {
+        private readonly SnackMachine snackMachine;
+        public override string Caption => "Snack Machine";
+
+        public SnackMachineViewModel(SnackMachine snackMachine)
+        {
+            this.snackMachine = snackMachine;
+        }
+    }
+}
