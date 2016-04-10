9d80ea88fbe121d1a0104cf7eb2a068d2745755a
tpodolak <tom.podolak@gmail.com>
2016-04-10 13:12:15 +0200

    Unit tests

--- DDD/DDDInPractice/DDDInPractice.Logic/Money.cs
+++ DDD/DDDInPractice/DDDInPractice.Logic/Money.cs
@@ -1,50 +1,67 @@
-﻿namespace DDDInPractice.Logic
+﻿using System;
+
+namespace DDDInPractice.Logic
 {
-	public class Money : ValueObject<Money>
-	{
-		public int OneCentCount { get; set; }
-		public int TenCentCount { get; set; }
-		public int QuaterCount { get; set; }
-		public int OneDollarCount { get; set; }
-		public int FiveDollarCount { get; set; }
-		public int TwentyDollarCount { get; set; }
+    public class Money : ValueObject<Money>
+    {
+        public int OneCentCount { get; set; }
+        public int TenCentCount { get; set; }
+        public int QuarterCount { get; set; }
+        public int OneDollarCount { get; set; }
+        public int FiveDollarCount { get; set; }
+        public int TwentyDollarCount { get; set; }
 
-		public Money(int oneCentCount, int tenCentCount, int quaterCount, int oneDollarCount, int fiveDollarCount, int twentyDollarCount)
-		{
-			OneCentCount = oneCentCount;
-			TenCentCount = tenCentCount;
-			QuaterCount = quaterCount;
-			OneDollarCount = oneDollarCount;
-			FiveDollarCount = fiveDollarCount;
-			TwentyDollarCount = twentyDollarCount;
-		}
+        public Money(int oneCentCount, int tenCentCount, int quarterCount, int oneDollarCount, int fiveDollarCount, int twentyDollarCount)
+        {
+            const string msg = "Value cannot be negative";
+            if (oneCentCount < 0)
+                throw new ArgumentException(msg, nameof(oneCentCount));
+            if (tenCentCount < 0)
+                throw new ArgumentException(msg, nameof(tenCentCount));
+            if (quarterCount < 0)
+                throw new ArgumentException(msg, nameof(quarterCount));
+            if (oneDollarCount < 0)
+                throw new ArgumentException(msg, nameof(oneDollarCount));
+            if (fiveDollarCount < 0)
+                throw new ArgumentException(msg, nameof(fiveDollarCount));
+            if (twentyDollarCount < 0)
+                throw new ArgumentException(msg, nameof(twentyDollarCount));
 
-		public static Money operator +(Money left, Money right)
-		{
-			return new Money(left.OneCentCount + right.OneCentCount, left.TenCentCount + right.TenCentCount, left.QuaterCount + right.QuaterCount,
-				left.OneDollarCount + right.OneDollarCount, left.FiveDollarCount + right.FiveDollarCount, left.TwentyDollarCount + right.TwentyDollarCount);
-		}
 
-		protected override int GetHashCodeInternal()
-		{
-			unchecked
-			{
-				int hashCode = base.GetHashCode();
-				hashCode = (hashCode*397) ^ OneCentCount;
-				hashCode = (hashCode*397) ^ TenCentCount;
-				hashCode = (hashCode*397) ^ QuaterCount;
-				hashCode = (hashCode*397) ^ OneDollarCount;
-				hashCode = (hashCode*397) ^ FiveDollarCount;
-				hashCode = (hashCode*397) ^ TwentyDollarCount;
-				return hashCode;
-			}
-		}
+            OneCentCount = oneCentCount;
+            TenCentCount = tenCentCount;
+            QuarterCount = quarterCount;
+            OneDollarCount = oneDollarCount;
+            FiveDollarCount = fiveDollarCount;
+            TwentyDollarCount = twentyDollarCount;
+        }
 
-		protected override bool EqualsInternal(Money other)
-		{
-			return OneCentCount == other.OneCentCount && TenCentCount == other.TenCentCount && QuaterCount == other.QuaterCount &&
-			       OneDollarCount == other.OneDollarCount && FiveDollarCount == other.FiveDollarCount && TwentyDollarCount == other.TwentyDollarCount;
-		}
+        public static Money operator +(Money left, Money right)
+        {
+            return new Money(left.OneCentCount + right.OneCentCount, left.TenCentCount + right.TenCentCount, left.QuarterCount + right.QuarterCount,
+                left.OneDollarCount + right.OneDollarCount, left.FiveDollarCount + right.FiveDollarCount, left.TwentyDollarCount + right.TwentyDollarCount);
+        }
 
-	}
+        protected override int GetHashCodeInternal()
+        {
+            unchecked
+            {
+                int hashCode = OneCentCount.GetHashCode();
+                hashCode = (hashCode * 397) ^ OneCentCount;
+                hashCode = (hashCode * 397) ^ TenCentCount;
+                hashCode = (hashCode * 397) ^ QuarterCount;
+                hashCode = (hashCode * 397) ^ OneDollarCount;
+                hashCode = (hashCode * 397) ^ FiveDollarCount;
+                hashCode = (hashCode * 397) ^ TwentyDollarCount;
+                return hashCode;
+            }
+        }
+
+        protected override bool EqualsInternal(Money other)
+        {
+            return OneCentCount == other.OneCentCount && TenCentCount == other.TenCentCount && QuarterCount == other.QuarterCount &&
+                   OneDollarCount == other.OneDollarCount && FiveDollarCount == other.FiveDollarCount && TwentyDollarCount == other.TwentyDollarCount;
+        }
+
+    }
 }
---
+++ DDD/DDDInPractice/DDDInPractice.Tests/MoneySpecs.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using DDDInPractice.Logic;
+using FluentAssertions;
+using Xunit;
+
+namespace DDDInPractice.Tests
+{
+    public class MoneySpecs
+    {
+        [Fact]
+        public void SumOfTwoMoneysProducesCorrectResultTest()
+        {
+            var left = new Money(1, 2, 3, 4, 5, 6);
+            var right = new Money(1, 2, 3, 4, 5, 6);
+            var result = left + right;
+            result.OneCentCount.Should().Be(2);
+            result.TenCentCount.Should().Be(4);
+            result.QuarterCount.Should().Be(6);
+            result.OneDollarCount.Should().Be(8);
+            result.FiveDollarCount.Should().Be(10);
+            result.TwentyDollarCount.Should().Be(12);
+        }
+
+        [Fact]
+        public void TwoMoneyInstancesEqualWhenContainsTheSameMoneyAmountTest()
+        {
+            var left = new Money(1, 2, 3, 4, 5, 6);
+            var right = new Money(1, 2, 3, 4, 5, 6);
+            left.Should().Be(right);
+            left.GetHashCode().Should().Be(right.GetHashCode());
+        }
+
+        [Fact]
+        public void TwoMoneyInstanceDoNotEqualWhenContainsDifferentMoneyAmmount()
+        {
+            var left = new Money(1, 0, 0, 0, 0, 0);
+            var right = new Money(0, 1, 0, 0, 0, 0);
+            left.Should().NotBe(right);
+            left.GetHashCode().Should().NotBe(right.GetHashCode());
+        }
+
+        [Theory]
+        [InlineData(-1, 0, 0, 0, 0, 0)]
+        [InlineData(0, -2, 0, 0, 0, 0)]
+        [InlineData(0, 0, -3, 0, 0, 0)]
+        [InlineData(0, 0, 0, -4, 0, 0)]
+        [InlineData(0, 0, 0, 0, -5, 0)]
+        [InlineData(0, 0, 0, 0, 0, -6)]
+        public void CannotCreateMoneyWithNegativeValue(int oneCentCount, int tenCentCount, int quarterCount, int oneDollarCount, int fiveDollarCount,
+            int twentyDollarCount)
+        {
+            Assert.Throws<ArgumentException>(
+                () => new Money(oneCentCount, tenCentCount, quarterCount, oneDollarCount, fiveDollarCount, twentyDollarCount));
+        }
+    }
+}
+
+
