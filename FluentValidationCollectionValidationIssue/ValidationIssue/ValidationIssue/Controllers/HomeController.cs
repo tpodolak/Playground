0fdd85bb1181cf28e6d713b7198088ac9186beae
tpodolak <misiek767@gmail.com>
2016-10-14 22:59:08 +0200

    FluentValidation collection validadtion issue

---
+++ FluentValidationCollectionValidationIssue/ValidationIssue/ValidationIssue/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ValidationIssue.ViewModels;
+
+namespace ValidationIssue.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var payments = new PaymentsViewModel(Enumerable.Range(0, 5).Select(val => new PaymentViewModel()));
+            return View(payments);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Pay(PaymentsViewModel payments)
+        {
+            var result = ModelState.IsValid;
+            if (result && payments.Count > 2 )
+                throw new InvalidOperationException("Validation was not called");
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FluentValidationCollectionValidationIssue/ValidationIssue/ValidationIssue/ViewModels/PaymentsViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Attributes;
+using FluentValidation.Results;
+
+namespace ValidationIssue.ViewModels
+{
+    [Validator(typeof(PaymentsViewModelValidator))]
+    public class PaymentsViewModel : List<PaymentViewModel>
+    {
+        public PaymentsViewModel()
+        {
+        }
+
+        public PaymentsViewModel(IEnumerable<PaymentViewModel> payments) : base(payments)
+        {
+        }
+
+    }
+
+    public class PaymentViewModel
+    {
+        public string Method { get; set; }
+    }
+
+    public class PaymentsViewModelValidator : AbstractValidator<PaymentsViewModel>
+    {
+        public PaymentsViewModelValidator()
+        {
+            RuleFor(val => val.Count).InclusiveBetween(1, 2);
+            RuleForEach(val => val).SetValidator(new PaymentViewModelValidator()).OverridePropertyName("Items");
+        }
+
+        public override ValidationResult Validate(PaymentsViewModel instance)
+        {
+            return base.Validate(instance);
+        }
+
+        public override ValidationResult Validate(ValidationContext<PaymentsViewModel> context)
+        {
+            return base.Validate(context);
+        }
+
+        public override Task<ValidationResult> ValidateAsync(ValidationContext<PaymentsViewModel> context)
+        {
+            return base.ValidateAsync(context);
+        }
+    }
+
+    public class PaymentViewModelValidator : AbstractValidator<PaymentViewModel>
+    {
+        public PaymentViewModelValidator()
+        {
+            RuleFor(val => val.Method).NotEmpty();
+        }
+
+        public override ValidationResult Validate(PaymentViewModel instance)
+        {
+            return base.Validate(instance);
+        }
+
+        public override Task<ValidationResult> ValidateAsync(ValidationContext<PaymentViewModel> context)
+        {
+            return base.ValidateAsync(context);
+        }
+
+        public override ValidationResult Validate(ValidationContext<PaymentViewModel> context)
+        {
+            return base.Validate(context);
+        }
+    }
+}
