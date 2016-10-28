d67e8d2b849db2c3974b760de622e77be1b4b6a3
tpodolak <misiek767@gmail.com>
2016-10-28 18:50:36 +0200

    Playin around with RedisSessionStateProvider

---
+++ AspNet/RedisSessionStateProvider/RedisSessionStateProvider/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.SessionState;
+using RedisSessionStateProvider.ViewModels;
+
+namespace RedisSessionStateProvider.Controllers
+{
+	[SessionState(SessionStateBehavior.Required)]
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+			TempData["Flight"] = new FlightViewModel
+			{
+				FlightNumber = Guid.NewGuid().ToString()
+			};
+			Session["MyCustomKey"] = new FlightViewModel
+			{
+				FlightNumber = Guid.NewGuid().ToString()
+			};
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = TempData["Flight"];
+
+			return View();
+		}
+	}
+}
---
+++ AspNet/RedisSessionStateProvider/RedisSessionStateProvider/ViewModels/FlightViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace RedisSessionStateProvider.ViewModels
+{
+	[Serializable]
+	public class FlightViewModel
+	{
+		[DataMember]
+		public string FlightNumber { get; set; }
+	}
+}
